4f9f378e6f893b0b09d33d49159c3921119a3adf
Artem Andreev <andreev.artem@gmail.com>
2011-07-28 15:46:00 +0400

    Initial Moodle 1.9 code

---
+++ lang/ja_utf8/qtype_order.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The language strings for the order question type.
+ *    
+ * @copyright &copy; 2007 Adriane Boyd
+ * @author adrianeboyd@gmail.com
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package aab_order
+ */
+
+$string['editingorder'] = '並び替え問題';
+$string['addingorder'] = '並び替え問題の追加';
+$string['order'] = '並び替え問題';
+$string['filloutthreeitems'] = '少なくとも３つ以上のアイテムを用意しなければなりません。空白のアイテムは無視されます。'; 
+$string['addmoreqblanks'] = 'もっとアイテムを追加する';
+$string['itemno'] = 'アイテム番号 $a';
+$string['horizontal'] = '横方向にアイテムを並べる';
+$string['defaultresponse'] = 'わかりません';
+
+?>
